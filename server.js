6f49e39f4677b72702828cbb6f3cc4dd19ba432f
Dayo Akinkuowo <me@sprypixels.com>
2018-04-17 18:53:22 +0100

    changes 3

--- server.js
+++ server.js
@@ -119,18 +119,14 @@ router.post('/articles/like/:id', (req, res) => {
 })
 
 // Update Article View
-router.put('/articles/view/:id', (req, res) => {
-  // Article.findOneAndUpdate({_id: req.params.id}, {
-  //   $inc : {
-  //     'article.likes' : 1
-  //   }
-  // })
-  Article.update({_id: req.params.id}, {
+router.get('/articles/view/:id', (req, res) => {
+  Article.findOneAndUpdate({_id: req.params.id}, {
     $inc : {
-      'article.views' : 1
+      'article.likes' : 1
     }
   }, (err) => {
     if(err) res.json({status: 'error'});
+    res.json({status: 'success'});
   });
 });
 
