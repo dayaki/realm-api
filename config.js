670046f5715e84f09a504c863f8790d308b587e2
Dayo Akinkuowo <me@sprypixels.com>
2018-05-05 19:25:22 +0100

    change db location

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   'secret':'realmauthkey',
-  // 'database': 'mongodb://dayaki:happy2day@ds255329.mlab.com:55329/realm',
-  'database': 'mongodb://localhost:27017/realm',
+  'database': 'mongodb://dayaki:happy2day@ds255329.mlab.com:55329/realm',
+  // 'database': 'mongodb://localhost:27017/realm',
   'port': process.env.PORT || 3000
 };
