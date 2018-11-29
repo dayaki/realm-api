591107f4761fa1b8a6803aece16b06b10f4fa531
Dayo Akinkuowo <me@sprypixels.com>
2018-11-29 21:24:52 +0100

    vouchers

--- server.js
+++ server.js
@@ -350,96 +350,43 @@ router.get('/admin/vouchers', (req, res) => {
   });
 });
 
-router.get('/admin/vouchers/new', (req, res) => {
+router.get('/admin/vouchers/new/:type', (req, res) => {
   // let today = moment();
   // let nextMonth = moment().add(1, 'months');
   // let expired = today.isSameOrBefore(nextMonth);
   // res.json({ today, nextMonth, expired })
-  // const type = req.body.type;
-  // if (type === 1) {
-  //   while (length < 20 ) {
-  //     let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  //     let voucher = new Voucher({
-  //       code: string,
-  //       expiry: moment().add(1, 'months'),
-  //       type: '1 Month'
-  //     });
-  
-  //     voucher.save((err, vou) => {
-  //       if (err) res.json({ status: 'error' })
-  //     });
-  
-  //     length = length + 1;
-  //   }
-  // } else if (type === 3) {
-  //   while (length < 20 ) {
-  //     let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  //     let voucher = new Voucher({
-  //       code: string,
-  //       expiry: moment().add(3, 'months'),
-  //       type: '3 Months'
-  //     });
-  
-  //     voucher.save((err, vou) => {
-  //       if (err) res.json({ status: 'error' })
-  //     });
-  
-  //     length = length + 1;
-  //   }
-  // } else if (type === 6) {
-  //   while (length < 20 ) {
-  //     let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  //     let voucher = new Voucher({
-  //       code: string,
-  //       expiry: moment().add(6, 'months'),
-  //       type: '6 Months'
-  //     });
-  
-  //     voucher.save((err, vou) => {
-  //       if (err) res.json({ status: 'error' })
-  //     });
-  
-  //     length = length + 1;
-  //   }
-  // } else if (type === 12) {
-  //   while (length < 20 ) {
-  //     let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-  //     let voucher = new Voucher({
-  //       code: string,
-  //       expiry: moment().add(12, 'months'),
-  //       type: '1 Year'
-  //     });
-  
-  //     voucher.save((err, vou) => {
-  //       if (err) res.json({ status: 'error' })
-  //     });
-  
-  //     length = length + 1;
-  //   }
+  res.json({ data: req.params.type })
+  // let type = '', expiry = '', length = 0;
+  // if (req.body.type === 1) {
+  //   expiry = moment().add(1, 'months').toISOString();
+  //   type = '1 Month';
+  // } else if (req.body.type === 3) {
+  //   expiry = moment().add(3, 'months').toISOString();
+  //   type = '3 Months';
+  // } else if (req.body.type === 6) {
+  //   expiry = moment().add(6, 'months').toISOString();
+  //   type = '6 Months';
+  // } else if (req.body.type === 12) {
+  //   expiry = moment().add(12, 'months').toISOString();
+  //   type = '1 year';
   // }
 
-  let length = 0;
-  while (length < 20 ) {
-    let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
-    let voucher = new Voucher({
-      code: string,
-      expiry: moment().add(1, 'months'),
-      type: '1 Month'
-    });
+  // while (length < 20 ) {
+  //   let string = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+  //   let voucher = new Voucher({
+  //     code: string.toUpperCase(),
+  //     expiry,
+  //     type
+  //   });
 
-    voucher.save((err, vou) => {
-      if (err) res.json({ status: 'error' })
-    });
+  //   voucher.save((err, vou) => {
+  //     if (err) res.json({ status: 'error' })
+  //   });
 
-    length = length + 1;
-  }
+  //   length = length + 1;
+  // }
 
-  res.json({ status: 'success' })
-
-  // Voucher.find({ used: false}, (err2, vouchers) => {
-  //   if (err2) res.json({ status: 'error', data: err2 })
-  //   res.json({ status: 'success', data: vouchers })
-  // });
+  // res.json({ status: 'success' });
 });
 
 // Fetch all attendance
