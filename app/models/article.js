af33944dd6c1589b36ffb703c03c5ce3f87f09f8
Dayo Akinkuowo <me@sprypixels.com>
2018-12-27 15:07:47 +0100

    members

--- app/models/article.js
+++ app/models/article.js
@@ -1,17 +1,18 @@
-let mongoose = require('mongoose');
-let Schema   = mongoose.Schema;
-let moment   = require('moment');
- 
+let mongoose = require("mongoose");
+let Schema = mongoose.Schema;
+let moment = require("moment");
+
 var ArticleSchema = new Schema({
   title: String,
   slug: String,
   image: String,
   author: String,
   color: String,
+  mdob: Date,
   created_at: {
     type: Date,
     default: moment()
   }
 });
 
-module.exports = mongoose.model('Article', ArticleSchema);
+module.exports = mongoose.model("Article", ArticleSchema);
